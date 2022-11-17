c8bce88ed580be7e4bf65c6f403bddc8e6fe6cb9
ViktorKlehta <113059807+ViktorKlehta@users.noreply.github.com>
2022-11-17 07:43:33 +0300

    Add files via upload

---
+++ JS5/index5.js
@@ -0,0 +1,23 @@
+
+
+
+//Функция отображения PopUp
+function PopUpShow(){
+    let stateObj = {
+        index: "bar",
+    }
+    console.log("hi");
+    history.pushState(null, 'page 2', 'bar.html');
+    console.log("hi2");
+    $("#popup1").show();
+}
+
+
+
+function openForm() {
+    document.getElementById("popup1").style.display="block";
+}
+
+
+
+
